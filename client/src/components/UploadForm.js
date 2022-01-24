ab1e9b5dafac976373b7e034d54374bf0de84ee7
Sangmin <bbsm8671@icloud.com>
2022-01-24 20:16:33 +0900

    remove .git files

---
+++ client/src/components/UploadForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import axios from "axios";                  //
+import { toast } from "react-toastify";     //error massage 출력
+import "./UploadForm.css";
+import ProgressBar from './ProgressBar';
+
+const UploadForm = () => {
+    const defaultFileName = "이미지 파일을 업로드 해주세요.";
+    const [file, setFile] = useState(null);
+    const [imgSrc, setImgSrc] = useState(null);
+    const [fileName, setFileName] = useState(defaultFileName);
+    const [percent, setPercent] = useState(0);
+
+    const imageSelectHandler = (event) => {
+        const imageFile = event.target.files[0];
+        //console.log({imageFile});
+        setFile(imageFile);
+        setFileName(imageFile.name);
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(imageFile);
+        fileReader.onload = e => setImgSrc(e.target.result);
+    };
+    
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append("image", file)
+        try {
+            const res = await axios.post("/upload", formData, {
+                headers: {"Content-Type":"multipart/form-data"},
+                // 현재 이미지 업로드 진행상황 표현 함수
+                onUploadProgress: (e) => {
+                    //console.log(ProgressEvent);
+                    setPercent(Math.round((100*e.loaded) / e.total));
+                },
+            });
+            //console.log({res});
+            toast.success("이미지 업로드 성공!");
+            // 이미지 업로드 성공 후 초기화
+            setTimeout(() => {
+                setPercent(0);                  //퍼센트 초기화
+                setFileName(defaultFileName);   //파일명 초기화
+                setImgSrc(null);                //이미지 초기화
+            }, 3000);
+        } catch(err) {
+            //alert("fail!!");
+            toast.error(err.message);
+            setPercent(0);
+            setFileName(defaultFileName);
+            console.error(err);
+        }
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <img src={imgSrc} className={`image-preview ${imgSrc && "image-preview-show"}`} />
+            <ProgressBar percent={percent}/>
+            <div className="file-dropper">
+                {fileName}
+                <input 
+                id="image" 
+                type="file" 
+                accept="image/*" //이미지 파일만 업로드 설정
+                onChange={imageSelectHandler} 
+                />
+            </div>
+        <button type="submit" style={{width:"100%", height:40, borderRadius:3, cursor:'pointer'}}> 제출 </button>
+      </form>
+    );
+};
+
+export default UploadForm;
