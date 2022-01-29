02b42eac3ff24919359c9e4015a47d99ee02803f
Sangmin <bbsm8671@icloud.com>
2022-01-29 23:59:40 +0900

    reactrouterdom

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import UploadForm from "./components/UploadForm";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import ImageList from "./components/ImageList";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import MainPage from "./pages/MainPage";
+import { Switch, Route } from "react-router-dom";
 
 const App = () => {
   // return 할 때 제일 최상 태그는 하나만 가능
   return (
-  <div style={{maxWidth: 600, margin: "auto"}}>
-      <ToastContainer /> 
-      <h2>사진첩</h2>
-      <UploadForm />
-      <ImageList />
-  </div>
+    <div style={{maxWidth: 600, margin: "auto"}}>
+        <ToastContainer /> 
+        <Switch>
+            <Route path="/" component={MainPage} />
+            <Route path="/auth/register" component={RegisterPage} />
+            <Route path="/auth/login" component={LoginPage} />
+        </Switch>
+    </div>
   );
 };
 
