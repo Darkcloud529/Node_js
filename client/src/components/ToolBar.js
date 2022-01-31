9d6554f25890a6445c52dffaae5331f33885f850
Sangmin <bbsm8671@icloud.com>
2022-01-31 20:03:01 +0900

    logout

--- client/src/components/ToolBar.js
+++ client/src/components/ToolBar.js
@@ -1,18 +1,32 @@
 import React, { useContext } from "react";
 import {  Link  } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import axios from "axios";
+import {toast} from "react-toastify"
 
 // Toolbar를 통해 다른 페이지 이동
 // link를 활용해 새로 고침이 아닌 원페이지 내에서 이동
 const ToolBar = () => {
     const [me, setMe] = useContext(AuthContext);
+
+    const logoutHandler = async () => {
+        try {
+            await axios.patch("/users/logout", {}, {headers:{sessionid: me.sessionId}});
+            setMe();
+            toast.success("로그아웃!");
+        } catch(err) {
+            console.log(err);
+            toast.error(err.message);
+        }
+    }
+
     return (
     <div>
         <Link to="/">
             <span> 홈 </span>
         </Link>
         {me ? 
-        (<span style={{float:"right"}}>로그아웃</span>) : (
+        (<span onClick={logoutHandler} style={{float:"right"}}>로그아웃</span>) : (
         <>
             <Link to="/auth/login">
                 <span style={{float: "right"}}> 로그인 </span>
