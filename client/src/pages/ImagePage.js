cb5aeabee8263841a7606a985530f4af10e9ccc9
Sangmin <bbsm8671@icloud.com>
2022-02-13 23:52:34 +0900

    ImageDelete

--- client/src/pages/ImagePage.js
+++ client/src/pages/ImagePage.js
@@ -3,8 +3,11 @@ import { useParams } from "react-router";
 import {ImageContext} from "../context/ImageContext"
 import { AuthContext } from "../context/AuthContext";
 import axios from "axios";
+import {toast} from "react-toastify";
+import {useNavigate} from "react-router-dom";
 
 const ImagePage = () => {
+    const navigate = useNavigate();
     const {imageId} = useParams(); 
     const { images, myImages, setImages, setMyImages } = useContext(ImageContext);
     const [me] = useContext(AuthContext);
@@ -16,14 +19,14 @@ const ImagePage = () => {
         if(me && image && image.likes.includes(me.userId)) setHasLiked(true);
     }, [me, image]);
     if(!image) return <h3>loading...</h3>;
-
+    //이미지 표시 함수
     const updateImage = (images, image) => [
         ...images.filter((image) => image._id !== imageId), image,
     ].sort(
         (a,b) => 
         new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
     );
-
+    // 좋아요 함수
     const onSubmit = async () => {
         const result = await axios.patch(`/images/${imageId}/${hasLiked ? "unlike" : "like"}`);
         if(result.data.public) {setImages(updateImage(images, result.data));}
@@ -31,11 +34,33 @@ const ImagePage = () => {
 
         setHasLiked(!hasLiked);
     };
+
+    // 이미지 삭제 함수
+    const deleteHandler = async() => {
+        try {
+            if(!window.confirm("정말 해당 이미지를 삭제하시겠습니까?")) return;
+            const result = await axios.delete(`/images/${imageId}`);
+            toast.success(result.data.message);
+            setImages(images.filter((image) => image._id !== imageId));
+            setMyImages(myImages.filter((image) => image._id !== imageId))
+            navigate("/");
+        } catch(err) {
+            toast.error(err.message);
+        }
+    };
+
     return (
     <div>
         <h3>Image Page - {imageId} </h3>
         <img style={{width:"100%"}} alt={imageId} src={`http://localhost:5000/uploads/${image.key}`}/>
         <span> 좋아요 {image.likes.length} </span>
+        {me && image.user._id === me.userId && (
+            <button 
+                style={{float:"right", marginLeft: 10 }} 
+                onClick={deleteHandler}> 
+            삭제 
+            </button>
+            )}
         <button style={{float:"right"}} onClick={onSubmit}>
             {hasLiked ? "좋아요 취소" : "좋아요"}
         </button>
